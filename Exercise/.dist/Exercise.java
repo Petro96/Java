2c49801055c2036947a869dc76dbcb047ca6b4f1
Petro96 <petrovic.vlado96@gmail.com>
2021-09-28 13:20:39 +0200

    commited

---
+++ Exercise/.dist/Exercise.java
@@ -0,0 +1,27 @@
+public class Exercise{
+
+    // EXERCISEs
+
+        //ex245. Write a Java program which accepts students name, id, and marks and display the highest 
+        //score and the lowest score
+
+        //ex247.  Write a Java program which accepts three integers and check whether 
+        //sum of the first two given integers is greater than third one.
+
+        //ex170 Write a Java program to find the length of the longest consecutive 
+        //sequence of a given array of integers
+
+        //ex31 Write a program that accepts three numbers from the user and prints 
+        //"increasing" if the numbers are in increasing order, "decreasing" (SORTING can be)
+
+        //ex Create Employee an Manager classes , print information...
+
+        
+    public static void main(String args[]){
+        String name = "Vlado";
+        System.out.println("String "+name);
+
+
+    }
+
+}
--- Program.java
+++ Program.java
@@ -1,6 +1,12 @@
 // MY FIRST JAVA PROGRAM 
 
-// public class Program{
+// import java.util.Scanner;
+// import java.util.*;
+// import java.lang.*;
+// import java.io.*;
+
+
+ public class Program{
     
 //     public void Answer(){
 //         System.out.println("Thanks for asking,Im fine.");
@@ -13,6 +19,92 @@
 //         System.out.println("How are you ?");
 //         //Program.Answer(); // you must create a object to call some method from class...
 //     }
-// }
+ 
 
-// Constructors
+// Constructors -- creating an object
+// - no argument constructors
+// - parameter constructors
+    
+    // public static void main(String args[]){
+    //     String name = "Jeff";    
+    //     int age = 12;
+    //     boolean result = name instanceof String;
+    //     age++;
+    //     System.out.println("Result: "+result+"Age"+age);
+    // }
+
+    // loops, for,while,do while
+    /*
+        public static void main(String args[]){
+            int [] numbers = {2,34,56,12,10,1};
+            String [] names = {"Jack","Mary","Simon","Filip"};
+            int num=20;
+            int k=10;
+
+            for(int x : numbers){
+                System.out.println("element "+x);
+            }
+            System.out.println("\n");
+            for (String name : names ){
+                System.out.println("Name: "+name);
+            }
+            System.out.println("\n");
+            while(num > 5){
+                System.out.println("Numbers: "+num);
+                num--;
+            }
+            System.out.println("\n");
+            do{
+                System.out.println("value: "+k);
+                k++;
+            }while(k < 20);
+        } */
+
+        //Desition making,input function(Scanner)
+        // ex.5 
+        /*
+        public static void main(String args[]){
+            Scanner in = new Scanner(System.in);
+            int result;
+
+            System.out.println("Enter the number: ");
+            int num1=in.nextInt();
+
+            System.out.println("Enter the second number: ");
+            int num2=in.nextInt();
+
+            if((num1 >=10) && (num2>=10)){
+                result = num1*num2;
+            }
+            else{
+                result=0;
+            }
+            
+
+            System.out.println("Multiplay of "+num1+" and "+num2+" is "+result);
+        }*/
+
+        // ex.33 sum of number digits
+
+        /*
+        public static int sumDigits(long num){
+            int sum=0;
+
+            while(num!=0){
+                sum+=num%10; // (num/10)%10
+                num/=10;
+            }
+            return sum;
+        }
+        
+        public static void main(String args[]){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter the number: ");
+            long n = in.nextLong();
+            System.out.println("Sum of digits is: "+sumDigits(n));
+        }
+        */
+
+        
+
+ }
